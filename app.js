2ac08f3c6229135434fa6b9d842e2a7219188843
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-20 13:27:32 +0100

    feat(item): create structure for item model

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+/*———————————————————————————————————*\
+    $ Routes
+\*———————————————————————————————————*/
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/item');
 
 const app = express();
 
@@ -12,7 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+/*———————————————————————————————————*\
+    $ Routes
+\*———————————————————————————————————*/
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/items', usersRouter);
 
 module.exports = app;
