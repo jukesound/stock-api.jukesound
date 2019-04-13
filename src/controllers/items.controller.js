1203fd4b2bb604086a171df292a442f7989b65e5
Vincent Battez <vincent.battez.pro@gmail.com>
2019-04-13 22:37:20 +0200

    style(item): add comment

--- src/controllers/items.controller.js
+++ src/controllers/items.controller.js
@@ -4,64 +4,63 @@ import config from 'config';
 
 class ItemController {
   static async all (req, res) {
+    // select all items
     const body = await ItemsModel.findAll();
 
+    // send all items
     res.status(config.httpCode.ok).json(body);
   }
   static async get (req, res) {
+    // select item
     const body = await ItemsModel.findOne({
       where: {
         id: req.params.id,
       },
     });
+
+    // send item selected
     res.status(config.httpCode.ok).json(body);
   }
   static async post (req, res) {
-    let body;
+    // Add slug
     const mutableBody = await Slug.addSlug(req.body);
+    // Validate
     await ItemsModel.validators(mutableBody, ItemsModel.schemaDefault());
 
-    body = await ItemsModel.create(mutableBody);
+    // POST new item in db
+    const body = await ItemsModel.create(mutableBody);
+    // send item created
     res.status(config.httpCode.created).json(body);
   }
   static async update (req, res) {
-    let item;
-    // Add slug
+    // Add slug in response
     const body = Slug.addSlug(req.body);
-
     // Validation
     await ItemsModel.validators(body, ItemsModel.schemaUpdate());
 
     // Select item
-    item = await ItemsModel.findOne({
+    const item = await ItemsModel.findOne({
       rejectOnEmpty: true,
       where: {
         id: req.params.id,
       },
     });
-
     // update in db
-    item = await item.update(body);
-
+    await item.update(body);
     // send item updated
     res.json(item);
   }
   static async delete (req, res) {
     // Select item
-    let item = await ItemsModel.findOne({
+    const item = await ItemsModel.findOne({
       rejectOnEmpty: true,
       where: {
         id: req.params.id,
       },
     });
 
-    // destroy item
-    await ItemsModel.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
+    // destroy item in db
+    await item.destroy();
     // send item destroyed
     res.status(config.httpCode.ok).json(item);
   }
