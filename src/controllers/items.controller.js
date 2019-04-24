851284290c5eee87450ae995c934db131b49107b
Vincent Battez <vincent.battez.pro@gmail.com>
2019-04-24 22:37:23 +0200

    docs(item): add comment

--- src/controllers/items.controller.js
+++ src/controllers/items.controller.js
@@ -3,6 +3,14 @@ import Slug from 'utils/Slug/Slug';
 import config from 'config';
 
 class ItemController {
+  /**
+   * Get all items
+   *
+   * @param {express.Request} req request
+   * @param {express.Response} res response
+   *
+   * @returns {ItemInterface[]}
+   */
   static async all (req, res) {
     // select all items
     const body = await ItemsModel.findAll();
@@ -10,6 +18,15 @@ class ItemController {
     // send all items
     res.status(config.httpCode.ok).json(body);
   }
+
+  /**
+   * Get items by ID
+   *
+   * @param {express.Request} req request
+   * @param {express.Response} res response
+   *
+   * @returns {ItemInterface}
+   */
   static async get (req, res) {
     // select item
     const body = await ItemsModel.findOne({
@@ -21,6 +38,15 @@ class ItemController {
     // send item selected
     res.status(config.httpCode.ok).json(body);
   }
+
+  /**
+   * Create new items
+   *
+   * @param {express.Request} req request
+   * @param {express.Response} res response
+   *
+   * @returns {ItemInterface}
+   */
   static async post (req, res) {
     // Add slug
     const mutableBody = await Slug.addSlug(req.body);
@@ -32,6 +58,15 @@ class ItemController {
     // send item created
     res.status(config.httpCode.created).json(body);
   }
+
+  /**
+   * Update item by ID
+   *
+   * @param {express.Request} req request
+   * @param {express.Response} res response
+   *
+   * @returns {ItemInterface}
+   */
   static async update (req, res) {
     // Add slug in response
     const body = Slug.addSlug(req.body);
@@ -50,6 +85,15 @@ class ItemController {
     // send item updated
     res.json(item);
   }
+
+  /**
+   * Delete item by ID
+   *
+   * @param {express.Request} req request
+   * @param {express.Response} res response
+   *
+   * @returns {ItemInterface}
+   */
   static async delete (req, res) {
     // Select item
     const item = await ItemsModel.findOne({
